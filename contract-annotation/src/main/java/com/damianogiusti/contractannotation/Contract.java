70a3fd772c0846419543c80a7cb013635db7b24b
Damiano Giusti <damiano.giusti@molo17.com>
2017-07-20 00:23:41 +0200

    add retention policy to annotation; add maven upload script

--- contract-annotation/src/main/java/com/damianogiusti/contractannotation/Contract.java
+++ contract-annotation/src/main/java/com/damianogiusti/contractannotation/Contract.java
@@ -1,6 +1,13 @@
 package com.damianogiusti.contractannotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by Damiano Giusti on 19/07/17.
  */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
 public @interface Contract {}
